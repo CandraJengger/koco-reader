6e6ecaa163b785ba2063908bde455f8f457cdfd3
CandraJengger <cakjengger@gmail.com>
2022-12-25 01:12:44 +0700

    ref: Format

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
-import templates from './templates/index.js';
+import {WIDGET_POSITION} from './constants/index.js';
+import {$} from './helpers/selector.js';
+import templates, {init} from './templates/index.js';
 
-export const createWidget = ({selector}) => {
-  const tempWrapper = document.querySelector(selector);
-  tempWrapper.innerHTML = templates;
+export const createWidget = ({
+  position = WIDGET_POSITION.Fixed,
+  selector = ''
+  // optionFixed = {
+  //   direction: 'right-bottom'
+  // }
+}) => {
+  const templateHTML = templates();
+
+  if (position === WIDGET_POSITION.Inline && selector) {
+    const tempWrapper = $(selector);
+    tempWrapper.innerHTML = templateHTML;
+
+    init();
+    return;
+  }
+
+  if (position === WIDGET_POSITION.Fixed) {
+    const bodyElem = $('body');
+    const wrapper = document.createElement('span');
+    wrapper.innerHTML = templateHTML;
+    bodyElem.append(wrapper);
+
+    init();
+    return;
+  }
 };
--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,23 @@
-import '../components/widget.js';
+import {on} from '../helpers/selector.js';
+import elements from './elements.js';
+import KocoWidget from '../components/KocoWidget.js';
 
-const templates = `
-  <koco-widget></koco-widget>
-`;
+const templates = () => KocoWidget();
+
+export function init() {
+  const {modalEl, volumeEl, playEl} = elements();
+
+  on(modalEl.toggle, 'click', function () {
+    modalEl.onToggle();
+  });
+
+  on(volumeEl.volumeBtn, 'click', function () {
+    volumeEl.onMute(true);
+  });
+
+  on(playEl.playPauseBtn, 'click', function () {
+    playEl.onPlay(true);
+  });
+}
 
 export default templates;
